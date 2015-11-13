ef258884449bdbc767b80fc041244ed3422d832b
Eric G <eric.gichuri@andela.com>
2015-11-13 18:03:09 +0300

    Test db data

--- web/sms.php
+++ web/sms.php
@@ -44,7 +44,7 @@ else {
 
 	//echo 'CON Test text '.$name.' '.$id.' '.$location;
 	// save the details to db
-	$sql = 'INSERT INTO farmer_farmer (name, location, id_number, phone_number) VALUES ($name, $location, $id, $phoneNumber)';
+	$sql = 'INSERT INTO farmer_farmer (name, location, id_number, phone_number) VALUES ('.$name.','.$location.','. $id.','. $phoneNumber.')';
 	$ret = pg_query($db, $sql);
 	if(!$ret){
 		echo 'END '.pg_last_error($db);
